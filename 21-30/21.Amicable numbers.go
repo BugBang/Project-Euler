7e698d72c138871c266d7a3e8dacee5c08baba27
bx0402 <ympbx@163.com>
2018-11-24 10:59:48 +0800

    updata

--- 21-30/21.Amicable numbers.go
+++ 21-30/21.Amicable numbers.go
@@ -1,69 +1,100 @@
+//package main
+//
+//import "fmt"
+//
+////亲和数
+////记d(n)为n的所有真因数（小于n且整除n的正整数）之和。
+////如果d(a) = b且d(b) = a，且a ≠ b，那么a和b构成一个亲和数对，a和b被称为亲和数。
+////
+////例如，220的真因数包括1、2、4、5、10、11、20、22、44、55和110，因此d(220) = 284；
+////而284的真因数包括1、2、4、71和142，因此d(284) = 220。
+////
+////求所有小于10000的亲和数的和。
+//func main() {
+//	var (
+//		amicableNum1 int
+//		amicableNum2 int
+//		result       []int
+//		sum          int
+//	)
+//	for i := 1; i < 100000; i++ {
+//		if amicableNum1 = isAmicableNum(i); amicableNum1 != -1 {
+//			if amicableNum2 = isAmicableNum(amicableNum1); amicableNum2 != -1 {
+//				if amicableNum1 != amicableNum2 && amicableNum2 == i {
+//					result = append(result, i)
+//					result = append(result, amicableNum2)
+//				}
+//			}
+//		}
+//	}
+//	result = removeDuplicateElement(result)
+//	for _, v := range result {
+//		sum += v
+//	}
+//	fmt.Println(sum)
+//}
+//
+//func removeDuplicateElement(amicableNums []int) []int {
+//	result := make([]int, 0, len(amicableNums))
+//	temp := map[int]struct{}{}
+//	for _, item := range amicableNums {
+//		if _, ok := temp[item]; !ok {
+//			temp[item] = struct{}{}
+//			result = append(result, item)
+//		}
+//	}
+//	return result
+//}
+//
+//func getSum(a []int) (result int) {
+//	for _, v := range a {
+//		result += v
+//	}
+//	return
+//}
+//
+//func isAmicableNum(a int) int {
+//	var (
+//		result []int
+//	)
+//	for i := 1; i < a; i++ {
+//		if a%i == 0 {
+//			result = append(result, i)
+//		}
+//	}
+//	if len(result) > 0 {
+//		return getSum(result)
+//	}
+//	return -1
+//}
+
 package main
 
-import "fmt"
+import (
+	"math"
+)
 
-//亲和数
-//记d(n)为n的所有真因数（小于n且整除n的正整数）之和。
-//如果d(a) = b且d(b) = a，且a ≠ b，那么a和b构成一个亲和数对，a和b被称为亲和数。
-//
-//例如，220的真因数包括1、2、4、5、10、11、20、22、44、55和110，因此d(220) = 284；
-//而284的真因数包括1、2、4、71和142，因此d(284) = 220。
-//
-//求所有小于10000的亲和数的和。
 func main() {
-	var (
-		amicableNum1 int
-		amicableNum2 int
-		result       []int
-		sum          int
-	)
+	sum, j := 0, 0
 	for i := 1; i < 10000; i++ {
-		if amicableNum1 = isAmicableNum(i); amicableNum1 != -1 {
-			if amicableNum2 = isAmicableNum(amicableNum1); amicableNum2 != -1 {
-				if amicableNum1 != amicableNum2 && amicableNum2 == i {
-					result = append(result, i)
-					result = append(result, amicableNum2)
-				}
+		j = sumOfProperDivisors(i)
+		if i == sumOfProperDivisors(j) && i != j {
+			sum += i
+		}
+	}
+	println(sum)
+}
+
+func sumOfProperDivisors(input int) int {
+	limit, sum := int(math.Sqrt(float64(input))), 0
+	for i := 1; i <= limit; i++ {
+		if input%i == 0 {
+			if i == input/i {
+				sum = sum + i
+			} else {
+				sum = sum + i + (input / i)
 			}
 		}
 	}
-	result = removeDuplicateElement(result)
-	for _, v := range result {
-		sum += v
-	}
-	fmt.Println(sum)
-}
-
-func removeDuplicateElement(amicableNums []int) []int {
-	result := make([]int, 0, len(amicableNums))
-	temp := map[int]struct{}{}
-	for _, item := range amicableNums {
-		if _, ok := temp[item]; !ok {
-			temp[item] = struct{}{}
-			result = append(result, item)
-		}
-	}
-	return result
-}
-
-func getSum(a []int) (result int) {
-	for _, v := range a {
-		result += v
-	}
-	return
-}
-
-func isAmicableNum(a int) int {
-	var (
-		result []int
-	)
-	for i := 1; i < a; i++ {
-		if a%i == 0 {
-			result = append(result, i)
-		}
-	}
-	if len(result) > 0 {
-		return getSum(result)
-	}
-	return -1
+	return sum - input
 }
--- 21-30/22.Names score.go
+++ 21-30/22.Names score.go
@@ -1,11 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strings"
+)
+
 //姓名得分
-//在这个46K的文本文件names.txt（右击并选择“目标另存为……”）中包含了五千多个姓名。首先将它们按照字母序排列，然后计算出每个姓名的字母值，乘以它在按字母顺序排列后的位置，以计算出姓名得分。
+//在这个46K的文本文件names.txt（右击并选择“目标另存为……”）中包含了五千多个姓名。
+//首先将它们按照字母序排列，
+//然后计算出每个姓名的字母值，乘以它在按字母顺序排列后的位置，以计算出姓名得分。
 //
-//例如，按照字母序排列后，位于第938位的姓名COLIN的字母值是3 + 15 + 12 + 9 + 14 = 53。因此，COLIN的姓名得分是938 × 53 = 49714。
+//例如，按照字母序排列后,
+//位于第938位的姓名COLIN的字母值是3 + 15 + 12 + 9 + 14 = 53。
+//因此，COLIN的姓名得分是938 × 53 = 49714。
 //
 //文件中所有姓名的姓名得分之和是多少？
 func main() {
+
+	var (
+		score int
+		sum   int
+	)
+
+	content, err := ioutil.ReadFile("code_kata/Project-Euler/21-30/22data")
+	if err != nil {
+		println("Error reading file")
+		return
+	}
+	names := strings.Split(strings.Replace(string(content), "\"", "", -1), ",")
+	sort.Strings(names)
+	for i, v1 := range names {
+		score = 0
+		for _, v2 := range v1 {
+			score += int(v2) - int('A') + 1
+		}
+		sum += score * (i + 1)
+	}
+
+	fmt.Println(sum)
 
 }
