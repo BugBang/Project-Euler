41c5cc1c56efad64014074d9c450c09744653070
bx0402 <ympbx@163.com>
2018-11-29 09:59:20 +0800

    updata

--- 21-30/26.Reciprocal cycles.go
+++ 21-30/26.Reciprocal cycles.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 //倒数的循环节
 //
 //单位分数指分子为1的分数。分母为2至10的单位分数的十进制表示如下所示：
@@ -19,4 +21,54 @@ package main
 //找出正整数d < 1000，其倒数的十进制表示小数部分有最长的循环节。
 func main() {
 
+	var (
+		numerator int
+		decimals  map[int]int
+		zeroCount int
+		temp      int
+		result    int
+	)
+
+	for i := 2; i < 1000; i++ {
+		zeroCount = 0
+		numerator = 1
+		decimals = make(map[int]int)
+		for {
+			if numerator < i {
+				numerator *= 10
+			} else {
+				break
+			}
+		}
+		if numerator%i == 0 {
+			continue
+		} else {
+			numerator = 1
+		}
+
+		for {
+			if numerator < i {
+				numerator *= 10
+				if numerator < i {
+					zeroCount++
+				}
+			} else {
+				break
+			}
+		}
+
+		for {
+			key := numerator - numerator/i*i
+			if decimals[key]-1 == 0 {
+				if len(decimals)+zeroCount > temp {
+					temp = len(decimals) + zeroCount
+					result = i
+				}
+				break
+			}
+			decimals[key] = 1
+			numerator = key * 10
+		}
+	}
+	fmt.Println(result)
 }
